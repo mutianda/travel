9bc2805bf3e526643da20db2ff4d3085d6c204d9
mutianda <qingyi.zongbu@qq.com>
2018-08-14 21:40:41 +0800

    header

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import fastClick from 'fastclick'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/conponents/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="header">
+  <div class="h-left"><span class="iconfont back-icon">&#xe624;</span></div>
+  <div class="h-input"><span class="iconfont search-icon">&#xe632;</span>景点/酒店/机票</div>
+  <div class="h-right">城市<span class="iconfont down-icon">&#xe62b;</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/varibles.styl"
+  .header
+    line-height :.86rem
+    display :flex
+    background $bgColor
+    color :#ffffff
+    .h-left
+      width :.6rem
+      float :left
+      text-align center
+      display inline-block
+      .back-icon
+        font-size .3rem
+        text-align center
+    .h-input
+      flex :1
+      height :.64rem
+      line-height .64rem
+      margin-top :.12rem
+      padding-left :.2rem
+      margin-left: .2rem;
+      background-color :#ffffff
+      border-radius :.1rem
+      color darkgray
+      display inline-block
+    .h-right
+      width :1.24rem
+      float :right
+      text-align :center
+      display inline-block
+      font-size .3rem
+      .down-icon
+        font-size .35rem
+        padding-left .1rem
+</style>
