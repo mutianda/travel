5c1bf4fe727f54ad19e8f60ed60aa106f9ed1506
mutianda <qingyi.zongbu@qq.com>
2018-08-17 07:28:58 +0800

    citysearch

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="sinput" type="text" placeholder="  please enter the key words">
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .search
+    border-top  #cacaca solid .01rem
+    background-color black
+    height .86rem
+    .sinput
+      background-color white
+      color #555555
+      width 80%
+      margin-left 10%
+      line-height .6rem
+      font-size .35rem
+      margin-top .11rem
+      text-align center
+      border-radius .1rem
+</style>
